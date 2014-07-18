e4cca15feb87b0b403d0947eda85d8bbda1af347
spavlick <spavlick@mit.edu>
2014-07-18 12:40:23 -0400

    Added plotting for entropy and divergence

--- src/utils/source-converge.py
+++ src/utils/source-converge.py
@@ -9,19 +9,39 @@ import matplotlib.pyplot as plt
 directory = '../../examples/pincell/'
 filenames = [f for f in os.listdir(directory) if '.h5' in f]
 h5_rm = [f.strip('.h5') for f in filenames]
-entropies = numpy.zeros(max([int(f.strip('statepoint.')) for f in h5_rm]))
-kl_divs = numpy.zeros(max([int(f.strip('statepoint.')) for f in h5_rm])-1)
+batch_nums = [int(f.strip('statepoint.')) for f in h5_rm]
+batch_nums = sorted(batch_nums,key=int) #sort in ascending order
+entropies = numpy.zeros(max(batch_nums))
+kl_divs = numpy.zeros(max(batch_nums)-1)
+ord_filenames = numpy.zeros(max(batch_nums))
+for i in range(len(ord_filenames)):
+  ord_filenames[i] = 'statepoint.' + batch_nums[i] + '.h5'
+
 
 
 mesh_cells = raw_input('Enter number of mesh cells in each direction as a tuple in the form (x,y,z)')
-mesh_size = raw_input('Enter the dimensions of one mesh cell as a tuple in the form (x,y,z)')
+mesh_size = raw_input('Enter the dimensions of the mesh as a tuple in the form (x,y,z)')
 mesh_center = raw_input('Enter the centerpoint of the mesh as a tuple in the form (x,y,z)')
+
+#calculate dimensions for a single cell
+(num_x,num_y,num_z) = mesh_cells
+(whole_x_width,whole_y_width,whole_z_width) = mesh_size
+x_width = whole_x_width/float(num_x)
+y_width = whole_y_width/float(num_y)
+z_width = whole_z_width/float(num_z)
+
+#find lower left of mesh
+(x_center,y_center,z_center)=mesh_center
+x_left = x_center-((num_x/2)*x_width)
+y_left = y_center-((num_y/2)*y_width)
+z_left = z_center-((num_z/2)*z_width)
+lower_left = (x_left,y_left,z_left)
 
 prev_probs = None
 cur_probs = None
 
 
-for filename in filenames:
+for index, filename in enumerate(ord_filenames):
   f = h5py.File(directory + filename,'r')
   positions = f['source_bank']['xyz']
   num_neutrons = f['n_particles']
@@ -39,10 +59,7 @@ for filename in filenames:
   fig.savefig('batch'+batch_num+'.png')
 
   #calculating mesh probabilities
-  (num_x,num_y,num_z) = mesh_cells
-  (x_width,y_width,z_width) = mesh_size
   cur_probs = numpy.zeros((mesh_cells))
-  lower_left = () #find way of calculating lower left
   (last_x,last_y,last_z)=lower_left
   (cur_x,cur_y,cur_z)= lower_left
   for i in range(num_x):
@@ -67,7 +84,7 @@ for filename in filenames:
     for j in range(num_y):
       for k in range(num_z):
         entropy+=cur_probs[i][j][k]*math.log(cur_probs[i][j][k])
-  entropies[index]=entropy #must initialize index
+  entropies[index]=entropy 
 
   #calculating KL Divergence
   if prev_probs != None:
@@ -84,7 +101,7 @@ for filename in filenames:
 
 #plotting Shannon entropies
 fig = plt.figure()
-plt.plot(range(len(entropies)),entropies,'o-')
+plt.plot(batch_nums,entropies,'o-')
 plt.title('Shannon Entropy v Batch Number')
 plt.xlabel('Batch Number')
 plt.ylabel('Shannon Entropy')
@@ -94,7 +111,9 @@ fig.savefig('shannon-entropies.png')
 
 #plotting KL Divergence
 fig = plt.figure()
-plt.plot([e+1 for e in range(len(kl_divs))],kl_divs,'o-')
+kl_batches = copy(batch_nums)
+kl_batches.pop(0)
+plt.plot(kl_batches,kl_divs,'o-')
 plt.title('KL Divergence v Batch Number')
 plt.xlabel('Batch Number')
 plt.ylabel('KL Divergence')
